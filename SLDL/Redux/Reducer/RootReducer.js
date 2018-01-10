34e6e612af145017a542adfef872e178571459d5
gjjggg <1271719322@qq.com>
2018-01-10 13:33:03 +0800

    no message

---
+++ SLDL/Redux/Reducer/RootReducer.js
@@ -0,0 +1,9 @@
+/**
+ * Created by guohongan on 2017/12/23.
+ */
+import { combineReducers } from 'redux';
+
+import  LoginReducer from  './LoginReducer'
+export  default  RootReducer = combineReducers({
+    LoginReducer
+})
--- SLDL/Redux/Store/ConfigureStore.js
+++ SLDL/Redux/Store/ConfigureStore.js
@@ -1,3 +1,14 @@
 /**
  * Created by guohongan on 2017/12/22.
  */
+import {createStore , applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import RootReducer from '../Reducer/LoginReducer'
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+
+export default function configureStore(initialState){
+    return createStoreWithMiddleware( RootReducer, initialState);
+}
