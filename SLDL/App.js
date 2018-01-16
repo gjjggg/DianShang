4aaadf9f5fb2075a725b578cbcef5fddef90bd3e
gjjggg <1271719322@qq.com>
2018-01-16 17:32:36 +0800

    no message

--- SLDL/App.js
+++ SLDL/App.js
@@ -12,12 +12,40 @@ import {
   View
 } from 'react-native';
 
+import {
+    Scene,
+    Router,
+    Actions,
+    Reducer,
+    ActionConst,
+    Overlay,
+    Tabs,
+    Modal,
+    Drawer,
+    Stack,
+    Lightbox,
+} from 'react-native-router-flux'
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
+const  reducerCreate = params =>{
+  const  defaultReducer = new Reducer(params);
+  return (state,action) => {
+    return defaultReducer(state,action);
+  };
+};
+const getScrneStyle =()=>({
+   backgroundColor:
+});
+
+const router = (...props)=>(
+    <Reducer createReducer ={reducerCreate}
+    get/>
+)
 
 export default class App extends Component<{}> {
   render() {
